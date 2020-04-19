55b691d585e3f7a9235ef4447292efaf055dc838
sirldavis <sirldavis@yahoo.com>
2020-04-19 17:21:22 +0100

    merge react
    merged react deploy from my-app

---
+++ precache-manifest.435fb0a0882b9defc0b0e7103180f5f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c9205b0e50b665c8e4af3847eae843",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f53f96139195a192485",
+    "url": "/static/css/2.19b7558e.chunk.css"
+  },
+  {
+    "revision": "7cf4ff1464be19be6daa",
+    "url": "/static/css/main.d20154a0.chunk.css"
+  },
+  {
+    "revision": "7f53f96139195a192485",
+    "url": "/static/js/2.2acb74c7.chunk.js"
+  },
+  {
+    "revision": "b0d065fe436f11781d264e524c4d6a20",
+    "url": "/static/js/2.2acb74c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cf4ff1464be19be6daa",
+    "url": "/static/js/main.bac88f26.chunk.js"
+  },
+  {
+    "revision": "51070b6aa1eda8f44ee9",
+    "url": "/static/js/runtime-main.3f426634.js"
+  }
+]);
